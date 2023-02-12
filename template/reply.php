74200f2f046d08973d510fd416b3197974b1fe16
花露不水 <112305853+y2008227@users.noreply.github.com>
2023-02-12 15:08:38 +0800

    Add files via upload

---
+++ template/reply.php
@@ -0,0 +1,37 @@
+<form class="Cf" method="post"><p><textarea name="msg" id="Msg" placeholder="在这里输入内容... \n\n你可以使用 Markdown 语法 \n或者图片拖拽、粘贴图片文件" rows="4" required=""></textarea></p>
+    <p>
+        <button type="button" class="btn" id="replyBtn">回复 Orz</button>
+    </p>
+    <input type="hidden" name="aid" value="{aid}">
+    <ul class="smile c">
+        <li>ฅ(๑˙˙๑)ฅ</li>
+        <li>(≥ω≤)</li>
+        <li>(๑• ̀д•́ )✧</li>
+        <li>(* ￣＾￣)</li>
+        <li>(๑￫ܫ￩)</li>
+        <li>(/・ω・＼)</li>
+        <li>(　・ˍ・)</li>
+        <li>（￣▽￣）</li>
+        <li>(=・ω・=)</li>
+        <li> (/ω＼)</li>
+        <li>(〜￣△￣)〜</li>
+        <li>(･∀･)</li>
+        <li>(°∀°)ﾉ</li>
+        <li>╮(￣▽￣)╭</li>
+        <li>( ´_ゝ｀)</li>
+        <li>("▔□▔)/</li>
+        <li> (。・ω・)</li>
+        <li>（/TДT)/</li>
+        <li>(｡･ω･｡)</li>
+        <li>(ノ≧∇≦)ノ</li>
+        <li>(´･_･`)</li>
+        <li>(-_-#)</li>
+        <li>（￣へ￣）</li>
+        <li>ヽ(`Д´)ﾉ</li>
+        <li>(╯°口°)╯(┴—┴</li>
+        <li>_(:3」∠)_</li>
+        <li>٩͡[๏̯͡๏]۶</li>
+        <li>٩(×̯×)۶</li>
+        <li>´_ゝ｀</li>
+    </ul>
+</form>
